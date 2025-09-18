2e6e060aeadf7f83356fb3eef66d8a9d43a0e34b
EliotdelaCruz <encruz646@gmail.com>
2025-09-17 18:42:40 -0600

    Initial LibraryApp - Eliot delaCruz

---
+++ LibraryApp/LibraryApp.Console/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+bool exit = false;
+
+while (!exit)
+{
+    ShowMenu();
+    var input = Console.ReadLine();
+    if (string.IsNullOrWhiteSpace(input))
+    {
+        Console.WriteLine("Invalid input. Please enter a number.");
+        continue;
+    }
+    int choice;
+    if (!int.TryParse(input, out choice))
+    {
+        Console.WriteLine("Invalid input. Please enter a valid number.");
+        continue;
+    }
+    Console.WriteLine();
+    switch (choice)
+    {
+        case 1: ListItems(); break;
+        default: Console.WriteLine("Unknown option."); break;
+    }
+
+}
+
+Console.WriteLine("Goodbye!");
+static void ShowMenu()
+{
+    Console.WriteLine("=== Library Management System ===");
+    Console.WriteLine("1) List all items");
+    Console.WriteLine("---------------------------------");
+}
+static void ListItems()
+{
+    Console.WriteLine("=== NO items ===");
+}
