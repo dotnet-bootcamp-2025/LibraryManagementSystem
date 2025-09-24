06007e04b71136da99b6c1bafc3570629897f327
Gadiel Mar <gadielmar10@outlook.com>
2025-09-23 18:58:23 -0600

    Add LibraryController and project reference for services
    Introduces the `LibraryController` class in the WebAPI project,
    which utilizes `LibraryService` to handle HTTP GET requests for
    items. Also adds a project reference to `LibraryApp.Console`
    to enable access to the `LibraryService`.

---
+++ LibraryApp/LibraryApp.WebAPI/Controllers/LibraryController.cs
@@ -0,0 +1,18 @@
+﻿using LibraryApp.Console.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryApp.WebAPI.Controllers
+{
+    public class LibraryController : ControllerBase
+    {
+        private readonly LibraryService _libraryService = new();
+
+        [HttpGet("items")]
+        public IActionResult GetItems()
+        {
+            _libraryService.Seed();
+            var items = _libraryService;
+            return Ok(items);
+        }
+    }
+}
