152375303081a17efd339e499576844ad4d53ce2
xobett <cessarr862@gmail.com>
2025-09-24 12:24:25 -0600

    Add API project with Library Controller

--- LibraryApp/LibraryApp.Api/Controllers/LibraryController.cs
+++ LibraryApp/LibraryApp.Api/Controllers/LibraryController.cs
@@ -6,6 +6,15 @@ namespace LibraryApp.Api.Controllers
 {
     public class LibraryController : ControllerBase
     {
-        private readonly LibraryService _libraryService;
+        private readonly LibraryService _service = new();
+
+        [HttpGet("items")]
+        public IActionResult GetSeedData()
+        {
+            _service.Seed();
+            var items = _service.Items;
+            return Ok(items);
+
+        }
     }
 }
