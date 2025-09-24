6ffa398091afbf2ad8128424256854e8d1689352
Rafael Franco Moreno <rafael.franco91075@gmail.com>
2025-09-23 19:20:18 -0600

    Add Get method

---
+++ LibraryApp/LibraryApp.Api/Controllers/LibraryController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryApp.Api.Controllers
+{
+    public class LibraryController : ControllerBase
+    {
+        private readonly LibraryService _service = new();
+
+        //add GET to list all library items
+        [HttpGet("items")]  
+        public IActionResult GetItems()
+        {
+            _service.Seed(); // Seed data for demo purposes
+            var items = _service.Items;
+            return Ok(items);
+        }
+
+    }
+}
