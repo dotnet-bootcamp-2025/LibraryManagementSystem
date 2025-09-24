aa81b414db017c878efb68a89a0bdef7255d276b
EliotdelaCruz <encruz646@gmail.com>
2025-09-24 16:46:12 -0600

    Added POST to LibraryController

--- LibraryApp/LibraryApp.Api/Controllers/LibraryController.cs
+++ LibraryApp/LibraryApp.Api/Controllers/LibraryController.cs
@@ -1,4 +1,5 @@
-﻿using LibraryApp.Console.Services;
+﻿using LibraryApp.Console.Domain;
+using LibraryApp.Console.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LibraryApp.Api.Controllers
@@ -20,6 +21,20 @@ namespace LibraryApp.Api.Controllers
             _service.Seed(); // Seed data for demonstration
             var items = _service.Items;
             return Ok(items);
+        }
+
+        // Homework: Add POST to add a new book
+
+        [HttpPost("books")]
+
+        public IActionResult AddBook([FromBody] Book bookDto)
+        {
+            if (bookDto == null || string.IsNullOrWhiteSpace(bookDto.Title) || string.IsNullOrWhiteSpace(bookDto.Author))
+            {
+                return BadRequest("Invalid book data.");
+            }
+            var book = _service.AddBook(bookDto.Title, bookDto.Author, bookDto.Pages);
+            return CreatedAtAction(nameof(GetItems), new { id = book.Id }, book);
         }
 
     }
