46f8b698520ec3c2eeec7e45f45282069f418c25
Relics97 <reliz9797@gmail.com>
2025-09-24 16:43:30 -0600

    Added POST AddBook also Implemented class BookCreateDto

--- LibraryApp/LibraryApp.Api/Controllers/LibraryController.cs
+++ LibraryApp/LibraryApp.Api/Controllers/LibraryController.cs
@@ -3,6 +3,13 @@ using LibraryApp.Console.Services;
 
 namespace LibraryApp.Api.Controllers
 {
+    //Add DTO class for creating a book
+    public class BookCreateDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public int Pages { get; set; }
+    }
     public class LibraryController : ControllerBase
     {
         //private static readonly LibraryService _service = new();
@@ -22,5 +29,18 @@ namespace LibraryApp.Api.Controllers
             return Ok(items);
         }
 
+
+        // Homework:
+        // Add Post to add a new book
+        [HttpPost("items/books")]
+        public IActionResult AddBook([FromBody] BookCreateDto dto)
+        {
+
+            if (string.IsNullOrWhiteSpace(dto.Title) || string.IsNullOrWhiteSpace(dto.Author) || dto.Pages < 0)
+                return BadRequest("Invalid book data.");
+
+            var book = _service.AddBook(dto.Title, dto.Author, dto.Pages);
+            return CreatedAtAction(nameof(GetItems), new { id = book.Id }, book);
+        }
     }
 }
