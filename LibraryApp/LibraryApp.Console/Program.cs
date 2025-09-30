a5cf027a88521a91d76f1f4bcfd824693c3d2cd6
Andres Briseño <abrisenocristerna@gmail.com>
2025-09-30 09:09:00 -0600

    change(console): Modify some methods in Program

--- LibraryApp/LibraryApp.Console/Program.cs
+++ LibraryApp/LibraryApp.Console/Program.cs
@@ -5,10 +5,12 @@ public class Program
 {
     private static readonly List<LibraryItem> _items = new();
     private static readonly LibraryService _service = new();
+
     public static void Main()
     {
         Console.WriteLine("Library App!");
         _service.Seed();
+
         bool exit = false;
         while (!exit)
         {
@@ -26,19 +28,21 @@ public class Program
                 continue;
             }
             Console.WriteLine();
+
             switch (choice)
             {
                 case 1: ListItems(); break;
-                case 2: SearchItems(); break;
+                case 2: //SearchItems(); break;
                 case 3: AddBook(); break;
                 case 4: AddMagazine(); break;
-                case 5: ListMembers(); break;
+                case 5: //ListMembers(); break;
                 case 6: RegisterMember(); break;
-                case 7: BorrowItem(); break;
-                case 8: ReturnItem(); break;
+                //case 7: BorrowItem(); break;
+                //case 8: ReturnItem(); break;
                 case 0: exit = true; break;
                 default: Console.WriteLine("Unknown option."); break;
             }
+
             if (!exit)
             {
                 Console.WriteLine("\nPress any key to continue...");
@@ -46,8 +50,10 @@ public class Program
                 Console.Clear();
             }
         }
+
         Console.WriteLine("Goodbye!");
     }
+
     static void ShowMenu()
     {
         Console.WriteLine("=== Library Management System ===");
@@ -55,16 +61,15 @@ public class Program
         Console.WriteLine("2) Search items by title (TBD)");
         Console.WriteLine("3) Add Book");
         Console.WriteLine("4) Add Magazine");
-        Console.WriteLine("5) List Members");
-        Console.WriteLine("6) Add Member");
-        Console.WriteLine("7) Borrow item");
-        Console.WriteLine("8) Return item");
+        Console.WriteLine("6) Register member");
         Console.WriteLine("0) Exit");
         Console.WriteLine("---------------------------------");
     }
+
     static void ListItems()
     {
         if (_service.Items.Count == 0) { Console.WriteLine("No items."); return; }
+
         Console.WriteLine("Items:");
         foreach (var item in _service.Items)
         {
@@ -73,6 +78,7 @@ public class Program
             Console.WriteLine($"{item.Id}: {item.GetInfo()} [{status}]");
         }
     }
+
     static void AddBook()
     {
         var title = InputHelper.ReadText("Title");
@@ -80,16 +86,20 @@ public class Program
         var pages = InputHelper.ReadInt("Pages (0 if unknown)");
 
         var book = _service.AddBook(title, author, pages);
+
         Console.WriteLine($"Added: {book.GetInfo()} (Id={book.Id})");
     }
+
     static void AddMagazine()
     {
         var title = InputHelper.ReadText("Title");
         var issue = InputHelper.ReadInt("Issue number");
         var publisher = InputHelper.ReadText("Publisher");
 
-        //var _nextItemId = _items.Count > 0 ? _items.Max(i => i.Id) : 0;
-        var mag = _service.AddMagazine(title, issue, publisher);
+        var _nextItemId = _items.Count > 0 ? _items.Max(i => i.Id) : 0;
+        var mag = new Magazine(_nextItemId++, title, issue, publisher);
+        _items.Add(mag);
+
         Console.WriteLine($"Added: {mag.GetInfo()} (Id={mag.Id})");
     }
 
@@ -98,48 +108,5 @@ public class Program
         var name = InputHelper.ReadText("Member name");
         var member = _service.RegisterMember(name);
         Console.WriteLine($"Registered: {member}");
-    }
-
-    private static void ListMembers()
-    {
-        if (_service.Members.Count == 0) { Console.WriteLine("No members."); return; }
-        Console.WriteLine("Members: ");
-        foreach (var member in _service.Members)
-        {
-            Console.WriteLine($"{member.Name} (Borrowed: { member.BorrowedItems.Count })");
-        }
-    }
-
-    private static void SearchItems()
-    {
-        var term = InputHelper.ReadText("Search term");
-        Console.WriteLine($"Search results for \"{term}\":");
-        InputHelper.PrintList(_service.FindItems(term));
-    }
-
-    private static void BorrowItem()
-    {
-        ListMembers();
-        var idMember = InputHelper.ReadInt("Member id");
-        ListItems();
-        var idItem = InputHelper.ReadInt("Item id");
-        string resultMessage;
-
-        _service.BorrowItem(idMember, idItem, out resultMessage);
-
-        Console.WriteLine($"{resultMessage}");
-    }
-
-    private static void ReturnItem()
-    {
-        ListMembers();
-        var idMember = InputHelper.ReadInt("Member id");
-        ListItems();
-        var idItem = InputHelper.ReadInt("Item id");
-        string resultMessage;
-
-        _service.ReturnItem(idMember, idItem, out resultMessage);
-
-        Console.WriteLine($"{resultMessage}");
     }
 }
