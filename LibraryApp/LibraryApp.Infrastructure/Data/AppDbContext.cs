8122ed5675fa0f05d7106cf8bdb9ab6bd9df49b9
Andres Briseño <abrisenocristerna@gmail.com>
2025-10-06 22:38:08 -0600

    add(databse): Connection with DB

---
+++ LibraryApp/LibraryApp.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace LibraryApp.Infrastructure.Data
+{
+    public class AppDbContext: DbContext
+    {
+        DbSet<Domain.Entities.Member> Members { get; set; }
+
+        DbSet<Domain.Entities.LibraryItem> LibraryItems { get; set; }
+
+        DbSet<Domain.Entities.BorrowedItem> BorrowedItems { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
