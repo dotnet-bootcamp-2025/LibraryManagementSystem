4b0ddbbaaa818086250a46d0a339a2b2f30e2a20
Andres Briseño <abrisenocristerna@gmail.com>
2025-10-09 11:08:35 -0600

    create(domain): Enums and entities

---
+++ LibraryApp/LibrartApp.Domain/Entities/BorrowItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrartApp.Domain.Entities
+{
+    public class BorrowItem
+    {
+        public int Id { get; set; }
+        public int MemberId { get; set; }
+        public int LibraryItemId { get; set; }
+
+      public Member? Member { get; set; }
+        public LibraryItem? LibraryItem { get; set; }
+
+    }
+}
---
+++ LibraryApp/LibrartApp.Domain/Entities/Member.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibrartApp.Domain.Entities
+{
+    public class Member
+    {
+     public int Id { get; set; }
+     public required string Name { get; set; }
+     public List<BorrowItem> BorrowedItems { get; set; } = new();
+    }
+}
