547a5036a9ead36394bfc1a5ba4972b10f38c490
Andres Briseño <abrisenocristerna@gmail.com>
2025-10-09 16:49:09 -0600

    create(infrastructure): New Infrastructure for databases operations

---
+++ LibraryApp/LibraryApp.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,48 @@
+﻿
+using LibrartApp.Domain.Entities;
+using LibrartApp.Domain.Enums;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Member> Members { get; set; }
+        public DbSet<BorrowItem> BorrowItems { get; set; }
+        public DbSet<LibraryItem> LibraryItems { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+       protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           base.OnModelCreating(modelBuilder);
+
+           modelBuilder.Entity<Member>().HasData(
+               new Member { Id = 1, Name = "Alice Johnson" },
+               new Member { Id = 2, Name = "Bob Smith" },
+               new Member { Id = 3, Name = "Charlie Brown" }
+           );
+
+            modelBuilder.Entity<LibraryItem>().HasData(
+               new LibraryItem { Id = 1, Title = "The Great Gatsby", Author = "F. Scott Fitzgerald", Pages = 180, Type = (int)LibraryItemTypeEnum.Book },
+               new LibraryItem { Id = 2, Title = "1984", Author = "George Orwell", Pages = 328, Type = (int)LibraryItemTypeEnum.Book },
+               new LibraryItem { Id = 3, Title = "To Kill a Mockingbird", Author = "Harper Lee", Pages = 281, Type = (int)LibraryItemTypeEnum.Book },
+               new LibraryItem { Id = 4, Title = "Time Magazine - July 2023", IssueNumber = 7, Publisher = "Time USA, LLC", Type = (int)LibraryItemTypeEnum.Magazine },
+               new LibraryItem { Id = 5, Title = "National Geographic - August 2023", IssueNumber = 8, Publisher = "National Geographic Partners", Type = (int)LibraryItemTypeEnum.Magazine }
+               );
+
+
+
+        }
+    }
+}
+
+
---
+++ LibraryApp/LibraryApp.Infrastructure/Data/LibraryAppRepository.cs
@@ -0,0 +1,104 @@
+﻿using LibrartApp.Domain.Entities;
+using LibraryApp.Application.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp.Infrastructure.Data
+{
+    public class LibraryAppRepository : ILibraryAppRepository
+    { 
+
+        private readonly AppDbContext _context;
+        public LibraryAppRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddBorrowedItem(BorrowItem borrowedItem)
+        {
+            _context.BorrowItems.Add(borrowedItem);
+            _context.SaveChanges();
+        }
+
+        public void ReturnBorrowedItem(int borrowedItemId)
+        {
+            var bi = _context.BorrowItems.Find(borrowedItemId);
+            _context.BorrowItems.Remove(bi);
+            _context.SaveChanges();
+        }
+
+        public void AddLibraryItem(LibraryItem libraryItem)
+        {
+            _context.LibraryItems.Add(libraryItem);
+            _context.SaveChanges();
+        }
+
+        public void AddMember(Member member)
+        {
+            _context.Members.Add(member);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<LibraryItem> GetAllLibraryItems()
+        {
+            return _context.LibraryItems.ToList();
+        }
+
+        public IEnumerable<Member> GetAllMembers()
+        {
+            return _context.Members.ToList();
+
+            // Unfinished attemp for returning BorrowedItems
+            var query = from member in _context.Members
+                        join borrow in _context.BorrowItems on member.Id equals borrow.MemberId
+                        /*join libItem in _context.LibraryItems on borrow.LibraryItemId equals libItem.Id*/ into borrowedItems
+                        select new
+                        {
+                            Id = member.Id,
+                            Name = member.Name,
+                            BorrowedItems = (List<BorrowItem>)borrowedItems
+                        };
+
+            //foreach (var member in members)
+            //{
+            //    Console.WriteLine(member.Name);
+            //    foreach(var LibItem in member.BorrowedItems)
+            //    {
+            //        Console.WriteLine($"{LibItem.Id} : {LibItem.Title}");
+            //    }
+            //}
+            var members = (IEnumerable<Member>)query.ToList();
+            return members;
+        }
+
+        public LibraryItem? GetLibraryItem(int id)
+        {
+            return _context.LibraryItems.Find(id);
+
+            // NOTA: Forma de traer varios registros
+            //var x = _context.LibraryItems.Where(Li => Li.IsBorrowed && Li.Pages > 100).ToList();
+
+            //return x;
+        }
+
+        public Member? GetMemberById(int id)
+        {
+            return _context.Members.Find(id);
+        }
+
+        public BorrowItem? GetBorrowedItem(int memberId, int libraryItemId)
+        {
+            var bi = _context.BorrowItems.Where(bi => bi.MemberId == memberId && bi.LibraryItemId == libraryItemId).FirstOrDefault();
+            return bi;
+        }
+
+        public void UpdateLibraryItem(LibraryItem libraryItem)
+        {
+            _context.LibraryItems.Update(libraryItem);
+            _context.SaveChanges();
+        }
+    }
+}
