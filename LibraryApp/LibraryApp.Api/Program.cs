3dbf5fd816521969e3e2d3e7da8063ae4e6adecc
Andres Briseño <abrisenocristerna@gmail.com>
2025-10-09 11:09:01 -0600

    create(infrastructure):New Infrastructure

--- LibraryApp/LibraryApp.Api/Program.cs
+++ LibraryApp/LibraryApp.Api/Program.cs
@@ -1,6 +1,8 @@
 
-
+using LibraryApp.Infrastrucutre.Data;
 using LibraryApp.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,29 +11,23 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddEndpointsApiExplorer(); 
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-// Add CORS services
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", policy =>
-    {
-        policy.AllowAnyOrigin()
-              .AllowAnyMethod()
-              .AllowAnyHeader();
-    });
-});
-// Register LibraryService as singleton
-// 3 life-cycles: Singleton, Scoped, Transient
-builder.Services.AddScoped<ILibraryService, LibraryService>();
-//builder.Services.AddSingleton<ILibraryService, LibraryService>();
 
+//Configure DbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
+    );
+
+// Register LibraryService as a singleton
+// Pregunta Entrevista: cuales son los 3 life-cycles con los que puedes inyectar instancias (Singleton, Scoped(se crea instancia a traves de request de contexto), Transient(se crea una instancia y se tira, es la más volatil de los 3))
+//builder.Services.AddScoped<ILibraryAppRepository, LibraryAppRepository>();
+builder.Services.AddScoped<ILibraryService, LibraryService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    // Enable both OpenAPI and Swagger UI
     app.MapOpenApi();
     app.UseSwagger();
     app.UseSwaggerUI();
---
+++ LibraryApp/LibraryApp.Infrastrucutre/Data/AppDbContext.cs
@@ -0,0 +1,59 @@
+﻿using LibrartApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp.Infrastrucutre.Data
+{
+    public class AppDbContext : DbContext
+    {
+        DbSet<Member> Members { get; set; }
+        DbSet<BorrowItem> BorrowItems { get; set; }
+        DbSet<LibraryItem> LibraryItems { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    base.OnModelCreating(modelBuilder);
+
+        //    modelBuilder.Entity<Member>(entity =>
+        //    {
+        //        entity.HasKey(e => e.Id);
+        //        entity.Property(e => e.Name).IsRequired();
+        //        entity.HasMany(e => e.BorrowedItems)
+        //              .WithOne(b => b.Member)
+        //              .HasForeignKey(b => b.MemberId);
+        //    });
+
+        //    modelBuilder.Entity<LibraryItem>(entity =>
+        //    {
+        //        entity.HasKey(e => e.Id);
+        //        entity.Property(e => e.Title).IsRequired();
+        //        entity.Property(e => e.IsBorrowed).HasDefaultValue(false);
+        //        entity.HasMany(e => e.BorrowItems)
+        //              .WithOne(b => b.LibraryItem)
+        //              .HasForeignKey(b => b.LibraryItemId);
+        //    });
+
+
+        //    modelBuilder.Entity<BorrowItem>(entity =>
+        //    {
+        //        entity.HasKey(e => e.Id);
+        //        entity.HasOne(e => e.Member)
+        //              .WithMany(m => m.BorrowedItems)
+        //              .HasForeignKey(e => e.MemberId);
+        //        entity.HasOne(e => e.LibraryItem)
+        //              .WithMany(l => l.BorrowItems)
+        //              .HasForeignKey(e => e.LibraryItemId);
+        //    });
+
+
+        //}
+    }
+}
